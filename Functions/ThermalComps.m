2e184e30b6a009f361ac01040a06439120858af2
Matthew Hume <mtthwhm@gmail.com>
2022-12-19 15:16:25 -0500

    fix newlines

--- Functions/ThermalComps.m
+++ Functions/ThermalComps.m
@@ -1,1 +1,86 @@
-function ThermalComps(dt)  global mass;  global MAX_SUB;  global SPECIFICC_GAS;  global SPECIFICC_LIQ;  global MMASS;  global L_DENSITY;  global BULK_MOD;  global VAPPRESS;  global VAPGRAD;  global R_CONST;  global total_mass;  global vapor_mass;  global Q;  global Temp;  global Volume;  global Press;  global ANTIONE_A;  global ANTIONE_B;  ##1. compute average temp  AvgC = 0;	vap_press = 0;  for ii = 1:MAX_SUB    AvgC += ((vapor_mass(ii) * SPECIFICC_GAS(ii)) + ((mass(ii) - vapor_mass(ii)) * SPECIFICC_GAS(ii)));  endfor  if GetMass() > 0;    AvgC = AvgC / total_mass;    Temp = Q / AvgC / total_mass;  else    Temp = 0;  endif  ##2. Compute average Press  m_i = 0;		##mols	NV = 0;		##litres	PNV = 0;	tNV = 0;  for ii = 1:MAX_SUB    m_i += vapor_mass(ii) / MMASS(ii);    density = L_DENSITY(ii);##    if(ii == 1)##      density += 0.56 * Temp * Temp - 134.0 * Temp + 6900.0;##    elseif(ii == 2)##      density += 0.03333 * Temp * Temp - 4.3333 * Temp + 73.3333;##    endif    tNV = (mass(ii) - vapor_mass(ii)) / density;	##Units of L		NV += tNV;	##Units of L		PNV += tNV / BULK_MOD(ii);	##Units of L/Pa  endfor  m_i = -m_i * R_CONST * Temp; ##Units of L*Pa  NV = Volume - NV;	##Units of L  delta = delta = (NV * NV) - (4.0 * m_i * PNV); ##delta of quadric eq. P^2*PNV+ P*NV + m_i = 0  if PNV > 0    Press = (-NV + sqrt(delta)) / (2.0 * PNV);  else    Press = 0;  endif  for ii = 1:MAX_SUB    if(Temp <= 0.0)      vap_press = 0.0;    else     vap_press = exp(ANTIONE_A(ii) - (ANTIONE_B(ii) / Temp))*1E5; #this is vapor pressure of current substance    endif    #need to boil material if vapor pressure > pressure, otherwise condense    if vap_press > Press      Q += Boil(dt,ii);    else      Q += Condense(dt,ii);    endif  endforendfunction
+function ThermalComps(dt)
+
+  global mass;
+  global MAX_SUB;
+  global SPECIFICC_GAS;
+  global SPECIFICC_LIQ;
+  global MMASS;
+  global L_DENSITY;
+  global BULK_MOD;
+  global VAPPRESS;
+  global VAPGRAD;
+  global R_CONST;
+  global total_mass;
+  global vapor_mass;
+  global Q;
+  global Temp;
+  global Volume;
+  global Press;
+  global ANTIONE_A;
+  global ANTIONE_B;
+
+  ##1. compute average temp
+
+  AvgC = 0;
+	vap_press = 0;
+
+  for ii = 1:MAX_SUB
+    AvgC += ((vapor_mass(ii) * SPECIFICC_GAS(ii)) + ((mass(ii) - vapor_mass(ii)) * SPECIFICC_GAS(ii)));
+  endfor
+
+  if GetMass() > 0;
+    AvgC = AvgC / total_mass;
+    Temp = Q / AvgC / total_mass;
+  else
+    Temp = 0;
+  endif
+
+  ##2. Compute average Press
+  m_i = 0;		##mols
+	NV = 0;		##litres
+	PNV = 0;
+	tNV = 0;
+
+  for ii = 1:MAX_SUB
+    m_i += vapor_mass(ii) / MMASS(ii);
+    density = L_DENSITY(ii);
+##    if(ii == 1)
+##      density += 0.56 * Temp * Temp - 134.0 * Temp + 6900.0;
+##    elseif(ii == 2)
+##      density += 0.03333 * Temp * Temp - 4.3333 * Temp + 73.3333;
+##    endif
+
+    tNV = (mass(ii) - vapor_mass(ii)) / density;	##Units of L
+		NV += tNV;	##Units of L
+
+		PNV += tNV / BULK_MOD(ii);	##Units of L/Pa
+
+  endfor
+  m_i = -m_i * R_CONST * Temp; ##Units of L*Pa
+  NV = Volume - NV;	##Units of L
+
+  delta = delta = (NV * NV) - (4.0 * m_i * PNV); ##delta of quadric eq. P^2*PNV+ P*NV + m_i = 0
+
+  if PNV > 0
+    Press = (-NV + sqrt(delta)) / (2.0 * PNV);
+  else
+    Press = 0;
+  endif
+
+  for ii = 1:MAX_SUB
+    if(Temp <= 0.0)
+      vap_press = 0.0;
+    else
+     vap_press = exp(ANTIONE_A(ii) - (ANTIONE_B(ii) / Temp))*1E5; #this is vapor pressure of current substance
+    endif
+
+    #need to boil material if vapor pressure > pressure, otherwise condense
+    if vap_press > Press
+      Q += Boil(dt,ii);
+    else
+      Q += Condense(dt,ii);
+    endif
+
+  endfor
+
+endfunction
--- Functions/ThermalCompsPR.m
+++ Functions/ThermalCompsPR.m
@@ -1,3 +1,84 @@
-function ThermalCompsPR(dt)  global mass;  global MAX_SUB;  global SPECIFICC_GAS;  global SPECIFICC_LIQ;  global MMASS;  global L_DENSITY;  global BULK_MOD;  global VAPPRESS;  global VAPGRAD;  global R_CONST;  global total_mass;  global vapor_mass;  global Q;  global Temp;  global Volume;  global Press;  global ANTIONE_A;  global ANTIONE_B;  global PR_Ar;  global PR_Br;  global CRITICAL_T;  ##1. compute average temp  AvgC = 0;  Press = 0;  TotalVapPress = 0;  n = 0;  Tcr_equiv = 0;	  for ii = 1:MAX_SUB    AvgC += ((vapor_mass(ii) * SPECIFICC_GAS(ii)) + ((mass(ii) - vapor_mass(ii)) * SPECIFICC_LIQ(ii)));    n += mass(ii)/MMASS(ii);    Tcr_equiv += CRITICAL_T(ii) * mass(ii)/sum(mass);  endfor    Vm = Volume/n;      if GetMass() > 0;    AvgC = AvgC / total_mass;    Temp = Q / AvgC / total_mass;  else    Temp = 0;  endif    for ii = 1:MAX_SUB    TotalVapPress += GetVapPress(ii);
+function ThermalCompsPR(dt)
+
+  global mass;
+  global MAX_SUB;
+  global SPECIFICC_GAS;
+  global SPECIFICC_LIQ;
+  global MMASS;
+  global L_DENSITY;
+  global BULK_MOD;
+  global VAPPRESS;
+  global VAPGRAD;
+  global R_CONST;
+  global total_mass;
+  global vapor_mass;
+  global Q;
+  global Temp;
+  global Volume;
+  global Press;
+  global ANTIONE_A;
+  global ANTIONE_B;
+  global PR_Ar;
+  global PR_Br;
+  global CRITICAL_T;
+
+  ##1. compute average temp
+
+  AvgC = 0;
+  Press = 0;
+  TotalVapPress = 0;
+  n = 0;
+  Tcr_equiv = 0;
+	
+
+  for ii = 1:MAX_SUB
+    AvgC += ((vapor_mass(ii) * SPECIFICC_GAS(ii)) + ((mass(ii) - vapor_mass(ii)) * SPECIFICC_LIQ(ii)));
+    n += mass(ii)/MMASS(ii);
+    Tcr_equiv += CRITICAL_T(ii) * mass(ii)/sum(mass);
   endfor
-  [A, B, aii, bii] = GetPRCoeffAll();  [x1, x2, x3, ThreeRoots] = SolveCubic(-(1-B),(A-3*B^2-2*B),(-A*B+B^2+B^3));    aii *= 1E6;  bii *= 1E6;    if(ThreeRoots && Temp < Tcr_equiv)    #Mixture is either Liquid, VLE, or Vapor    Zl = x3+(1-B)/3;    Zv = x1+(1-B)/3;        Vv = Zv*R_CONST*Temp/TotalVapPress*1000;    Vl = Zl*R_CONST*Temp/TotalVapPress*1000;    if(Vm > Vv || Vm <Vl)      Press = (R_CONST/1000*Temp/(Vm-bii) - aii/((Vm*(Vm+bii)+bii*(Vm-bii))))*1E6;    else      Press = TotalVapPress;    endif  else    #Mixture is supercritical    Press = (R_CONST/1000*Temp/(Vm-bii) - aii/((Vm*(Vm+bii)+bii*(Vm-bii))))*1E6;  endif     ##  for ii = 1:1##    [A, B] = GetPRCoeff(ii);##    [x1, x2, x3] = SolveCubic(-(1-B),(A-3*B^2-2*B),(-A*B+B^2+B^3));##    (x1*R_CONST*Temp)/GetVapPress(1)##  endfor    endfunction
+    Vm = Volume/n;
+    
+  if GetMass() > 0;
+    AvgC = AvgC / total_mass;
+    Temp = Q / AvgC / total_mass;
+  else
+    Temp = 0;
+  endif
+  
+  for ii = 1:MAX_SUB
+    TotalVapPress += GetVapPress(ii);
+
+  endfor
+
+  [A, B, aii, bii] = GetPRCoeffAll();
+  [x1, x2, x3, ThreeRoots] = SolveCubic(-(1-B),(A-3*B^2-2*B),(-A*B+B^2+B^3));
+  
+  aii *= 1E6;
+  bii *= 1E6;
+  
+  if(ThreeRoots && Temp < Tcr_equiv)
+    #Mixture is either Liquid, VLE, or Vapor
+    Zl = x3+(1-B)/3;
+    Zv = x1+(1-B)/3;
+    
+    Vv = Zv*R_CONST*Temp/TotalVapPress*1000;
+    Vl = Zl*R_CONST*Temp/TotalVapPress*1000;
+
+    if(Vm > Vv || Vm <Vl)
+      Press = (R_CONST/1000*Temp/(Vm-bii) - aii/((Vm*(Vm+bii)+bii*(Vm-bii))))*1E6;
+    else
+      Press = TotalVapPress;
+    endif
+  else
+    #Mixture is supercritical
+    Press = (R_CONST/1000*Temp/(Vm-bii) - aii/((Vm*(Vm+bii)+bii*(Vm-bii))))*1E6;
+  endif
+   
+  
+##  for ii = 1:1
+##    [A, B] = GetPRCoeff(ii);
+##    [x1, x2, x3] = SolveCubic(-(1-B),(A-3*B^2-2*B),(-A*B+B^2+B^3));
+##    (x1*R_CONST*Temp)/GetVapPress(1)
+##  endfor
+    
+endfunction
